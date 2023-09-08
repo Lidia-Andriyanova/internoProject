9ff4e4acf3591aa9ce0cf25a08ccdadd362af92a
Lidia Andriyanova <lidia.andriyanova@mail.ru>
2023-09-08 17:14:58 +0300

    index news html

---
+++ src/components/NewsItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <img class="news-content__article__img" :src="require(`@/assets/${newsItem.img}`)" alt="Article"> 
+        <h4 class="news-content__article__title">{{ newsItem.title }}</h4>
+        <div class="news-content__info">
+            <p class="news-content__info__date">{{ newsItem.date }}</p>
+            <button class="news-content__info__next"><img :src="require(`@/assets/${nextImg}`)" alt="Next">                        
+            </button>
+        </div>
+        <div class="news-content__category">
+            <p class="news-content__category__text">{{ newsItem.category }}</p>
+        </div>         
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NewsItem',
+    props: ['newsItem'],
+
+    data() {
+        return {
+            nextImg: 'next.svg',
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="sass" module>
+
+</style>
---
+++ src/components/NewsList.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <NewsItem
+            v-for="newsItem in newsItems" 
+                :newsItem="newsItem"
+                :key="newsItem.id"
+        />
+    </div>
+</template>
+
+<script>
+import NewsItem from './NewsItem.vue'
+
+export default {
+    name: 'NewsList',
+    components: {
+        NewsItem,
+    },
+
+    data() {
+        return {
+            newsItems: [
+                    {
+                        id: 1,
+                        title: 'Let’s Get Solution For Building',
+                        date: '26 December,2023',
+                        category: 'Kitchan Design',
+                        img: 'article1.jpg',
+                    },
+                    {
+                        id: 2,
+                        title: 'Low Cost Latest Invented Interior Designing Ideas.',
+                        date: '22 December,2023',
+                        category: 'Living Design',
+                        img: 'article2.jpg',
+                    },
+                    {
+                        id: 3,
+                        title: 'Best For Any Office & Business Interior Solution',
+                        date: '25 December,2023',
+                        category: 'Interior Design',
+                        img: 'article3.jpg',
+                    },      
+                ]            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="sass" module>
+
+</style>
